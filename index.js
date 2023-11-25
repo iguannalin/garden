51facb1bdfd4502f600aaf28ca24864a2deed3a0
seasons986 <annalin12345@gmail.com>
2023-11-25 04:01:27 -0500

    blank

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ window.addEventListener("load", () => {
     const text = `<!DOCTYPE html><html> <head> <title>garden</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/garden/index.css"/><script src=https://iguannalin.github.io/garden/index.js></script></head> <body> <div id="container" data-garden=${btoa(garden)}></div></body></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
-    window.open(blobUrl, '_self');
+    window.open(blobUrl, '_blank');
     window.URL.revokeObjectURL(blobUrl);
   }
 
